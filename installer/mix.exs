d86a131b48e3338082ce51189cd2ed14839b6094
Eric Saxby <sax@geometer.io>
2021-09-27 11:48:02 -0700

    Bump to 1.2.0

--- installer/mix.exs
+++ installer/mix.exs
@@ -8,7 +8,7 @@ defmodule GenMedic.MixProject do
   """
   @medic_url "https://hex.pm/packages/medic"
   @scm_url "https://github.com/geometerio/medic"
-  @version "1.1.0"
+  @version "1.2.0"
 
   def project do
     [
