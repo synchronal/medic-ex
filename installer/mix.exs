2e771e11f229d0f9860afd40d889390153b21d5a
Eric Saxby <sax@livinginthepast.org>
2022-12-28 17:52:46 -0800

    Release v1.8.4

--- installer/mix.exs
+++ installer/mix.exs
@@ -16,7 +16,7 @@ defmodule GenMedic.MixProject do
   """
   @medic_url "https://hex.pm/packages/medic"
   @scm_url "https://github.com/synchronal/medic"
-  @version "1.8.3"
+  @version "1.8.4"
 
   def project do
     [
